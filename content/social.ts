59e43beca388023d073d0ebe380a63c20eece338
Anfas <anfas010101@gmail.com>
2026-07-30 11:13:31 +0530

    bug fixed

--- content/social.ts
+++ content/social.ts
@@ -1,14 +1,11 @@
-import { Github, Linkedin } from "lucide-react";
 
 export const socialLinks = [
   {
     label: "GitHub",
     href: "",
-    icon: Github,
   },
   {
     label: "LinkedIn",
     href: "",
-    icon: Linkedin,
   },
 ] as const;
